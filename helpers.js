fae467fdbaf6f8c5b5fcccc43dd8a061d6667085
Tania Tasmin <taniat.kaau@email.com>
2021-11-29 10:30:54 -0500

    Refactor sendSMS function

--- helpers.js
+++ helpers.js
@@ -2,17 +2,17 @@
 
 const sendSMS = function (phone_number, message){
 
-var twilio = require('twilio');
+  var twilio = require('twilio');
 
-// Find your account sid and auth token in your Twilio account Console.
-var client = new twilio('AC6642a5ff5c418d4047d13dcd6dd90a38', 'f9b073f566885e555d6e77a01762d8f9');
+  // Find your account sid and auth token in your Twilio account Console.
+  var client = new twilio('AC6642a5ff5c418d4047d13dcd6dd90a38', 'f9b073f566885e555d6e77a01762d8f9');
 
-// Send the text message.
-client.messages.create({
-  to: phone_number,
-  from: '12058556664',
-  body: message
-});
+  // Send the text message.
+  client.messages.create({
+    to: phone_number,
+    from: '12058556664',
+    body: message
+  });
 
 };
 
