218a2e875a8257d381b872fe4a0bb65c646b9f9d
Tania Tasmin <taniat.kaau@email.com>
2021-11-29 05:52:16 -0500

    Create the Jquery to extract datas from the from

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,27 @@
 // Client facing scripts here
 $(document).ready(function(){
   
-  $("form").submit(function(event){
-    
-    event.preventDefault();
-    var formValues = $(form).serializeArray();;
-    console.log(formValues);
-    
-  }); 
+  $("button").click(function(){
+    var item_Name = [];
+    var number_of_Items = [];
 
+    $.each($("input[name='itemName']:checked"), function(){
+      item_Name.push($(this).val());
+    });
+
+    $.each($("input[type='text']"), function(){
+        if ($(this).val()!==""){
+        number_of_Items.push($(this).val()); 
+      }
+  }) 
+  console.log(item_Name);
+  console.log(number_of_Items);
+  
+  alert(item_Name.join(", ")+ number_of_Items.join(", "));
 
 });
+
+});
+
+
 
