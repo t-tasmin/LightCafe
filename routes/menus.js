5f1e3cab2d269f2606048f4b1b56329d4f8d6b7c
Tania Tasmin <taniat.kaau@email.com>
2021-11-30 13:46:22 -0500

    Add a database query for unit price of selected fooditems

--- routes/menus.js
+++ routes/menus.js
@@ -28,40 +28,52 @@ module.exports = (db) => {
       });
   });
 
+  
   router.post("/", (req, res) => {
 
-    
-    let items = req.body.itemName;
-    console.log("Body***********",req.body);
+    let itemNames = req.body.itemName;
     let numberOfItems = req.body.numberOfItems;
     numberOfItems = numberOfItems.filter((a) => a); 
 
-    let itemNames=[];
-    let unitPrices=[];
-    let orders  = [];
-
-    for (let i in items){
-      let s= items[i].split("+");
-      itemNames.push(s[0]);
-      unitPrices.push(s[1]);
-    }
-
-    for (let index in itemNames)
-    {
-      orders[index] ={itemName: itemNames[index], unitPrice: unitPrices[index], numberOfItem: numberOfItems[index] ,totalPrice:numberOfItems[index]*unitPrices[index]};
-    }
-
-    console.log(orders);
-    let orderVar = {orders};
-    res.render("order_checkout",orderVar);
-   
-    queryParams = ['('+itemNames.join(',')+')'];
     let queryString = `
-    UPDATE menu_dishes
-    SET number_available = number_available -1
-    WHERE name in $1
+    SELECT unit_price
+    FROM menu_dishes
+    WHERE name in ( 
      `;
-    console.log(queryString, queryParams);
+     for (let i=0; i<itemNames.length; i++)
+     {
+       if (i === itemNames.length-1)
+       {queryString+=`'${itemNames[i]}');`;}
+       else
+       {queryString+=`'${itemNames[i]}', `;}
+     }
+    
+    console.log(queryString);
+    
+    db.query(queryString)
+      .then(data => {
+        const unitPriceObj = data.rows;
+        let unitPrices= unitPriceObj.map(function (element){
+          return element.unit_price;
+        });
+        
+        let orders  = [];
+        for (let index in itemNames)
+        {
+          orders[index] ={itemName: itemNames[index], unitPrice: unitPrices[index], numberOfItem: numberOfItems[index] ,totalPrice:numberOfItems[index]*unitPrices[index]};
+        }
+
+        console.log(orders);
+        let orderVar = {orders};
+        res.render("order_checkout",orderVar);
+                  
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+      
     
   });
 
