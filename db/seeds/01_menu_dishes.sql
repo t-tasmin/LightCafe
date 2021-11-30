ef5f52f1fb4c9eef593780cf19fa83a8970b624c
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-30 13:52:21 -0500

    Add prepTime for dishes

--- db/seeds/01_menu_dishes.sql
+++ db/seeds/01_menu_dishes.sql
@@ -14,13 +14,14 @@ INSERT INTO menu_dishes (
   name ,
   unit_price,
   number_available,
+  prep_time,
   restaurant_id
 )
-VALUES ('Curry Chicken',10,20,1),
-       ('Butter Chicken',15,20,1),
-       ('Goat Curry',15,20,1),
-       ('Fried Rice',8,20,1),
-       ('Vegetable Salad',9,20,1),
-       ('Fruits Salad',15,20,1);
+VALUES ('Curry Chicken',10,20,20,1),
+       ('Butter Chicken',15,20,16,1),
+       ('Goat Curry',15,20,22,1),
+       ('Fried Rice',8,20,10,1),
+       ('Vegetable Salad',9,20,5,1),
+       ('Fruits Salad',15,20,8,1);
 
 
