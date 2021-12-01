848f736fd83176e7dcb512ab385ddbaf94cea9e7
Tania Tasmin <taniat.kaau@email.com>
2021-12-01 14:24:12 -0500

    Comment demo route

--- server.js
+++ server.js
@@ -51,9 +51,9 @@ app.get("/", (req, res) => {
 
 //*******DEMO ROUTE FOR CHECKOUT_ORDER PAGE *************/
 
-app.get('/checkout', (req,res) => {
-  res.render("order_checkout");
-})
+// app.get('/checkout', (req,res) => {
+//   res.render("order_checkout");
+// })
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
