e09778fe74a20f3561db9e76ce65fef9a7672edb
Tania Tasmin <taniat.kaau@email.com>
2021-12-01 14:25:29 -0500

    Add GET route for orders and Render order checkout

--- routes/orders.js
+++ routes/orders.js
@@ -2,17 +2,19 @@
 
 const express = require('express');
 const router  = express.Router();
-const orderVar = require('./menus')
 
 module.exports = (db) => {
 
   //**************************GET ROUTE***************************/
-  router.get("/", (req, res) => {
-    //res.render("order_checkout",orderVar);
-    const time = '1:30 PM'
-    res.render("last",{time});
+  router.get("/:a", (req, res) => {
+    let orderVar = JSON.parse(req.params.a);
+    res.render("order_checkout", orderVar);
+    //const time = '1:30 PM'
+    //res.render("last",{time});
   });
 
+
+  
  //**************************POST ROUTE***************************/
   router.post("/", (req, res) => {
     
