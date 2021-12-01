d6d3dbd1e3a63360904ce648918583004dcb922a
Tania Tasmin <taniat.kaau@email.com>
2021-12-01 14:58:34 -0500

    Create GET route for orders

--- routes/orders.js
+++ routes/orders.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
+  
+ //**************************GET ROUTE***************************/
+ router.get("/:a", (req, res) => {
+  let orderVar = JSON.parse(req.params.a);
+  res.render("order_checkout", orderVar);
+  //const time = '1:30 PM'
+  //res.render("last",{time});
+});
+
+//**************************POST ROUTE***************************/
   router.post("/", (req, res) => {
     console.log('this order ===>', req.body) //==================================x
 
