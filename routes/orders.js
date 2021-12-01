e174b001297dd9ff1872ee9f54b0a2fe7c554256
Tania Tasmin <taniat.kaau@email.com>
2021-12-01 18:13:41 -0500

    Remove the code for last view

--- routes/orders.js
+++ routes/orders.js
@@ -7,8 +7,7 @@ module.exports = (db) => {
  router.get("/:a", (req, res) => {
   let orderVar = JSON.parse(req.params.a);
   res.render("order_checkout", orderVar);
-  //const time = '1:30 PM'
-  //res.render("last",{time});
+  
 });
 
 //**************************POST ROUTE***************************/
